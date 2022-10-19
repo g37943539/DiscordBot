b9f36dc49d6905b1ba98ec497c5fd8a0379381b5
Hsuan <sofa1269339@gmail.com>
2022-10-19 18:27:35 +0800

    Initial commit

--- main.py
+++ main.py
@@ -1,44 +1,24 @@
-import discord
-import json
-import random
-import asyncio
-#Python 3.10.8
+# This example requires the 'message_content' privileged intent to function.
 
-data = json.load(open('setting.json'))
+import discord
 
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('------')
-        print(f'{self.user}上線LOL')
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
 
     async def on_message(self, message):
-        # 讓他不要一直回自己的訊息
+        # we do not want the bot to reply to itself
         if message.author.id == self.user.id:
             return
-        # 嗨
-        if message.content.startswith('!嗨'):
-            await message.reply('銃三小 低能兒', mention_author=True)
-        # 猜數字
-        if message.content.startswith('!猜數字'):
-            await message.channel.send('1~10猜個數字')
-            def is_correct(m):
-                return m.author == message.author and m.content.isdigit()
-            answer = random.randint(1, 10)
-            try:#如果過5秒沒回答
-                guess = await self.wait_for('message', check=is_correct, timeout=5.0)
-            except asyncio.TimeoutError:#執行
-                return await message.channel.send(f'要不要打字啦 操 答案:{answer}')
 
-            if int(guess.content) == answer:
-                await message.channel.send('對了啦 阿不就好棒棒')
-            else:
-                await message.channel.send(f'笨小孩是{answer}啦幹')
+        if message.content.startswith('!hello'):
+            await message.reply('Hello!', mention_author=True)
 
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
-client.run(data['TOKEN'])
+client.run('MTAyNzE1MTU4NDk0NzM1MTU2Mg.GYEtLc.bIdiWjmLDZUXp7RY76Nr1CmvrTblwKMfykLVsQ')
