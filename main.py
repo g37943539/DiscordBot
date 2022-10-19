ab995b0bd657f388218be030f90bd54aa475814a
Hsuan <sofa1269339@gmail.com>
2022-10-19 18:57:10 +0800

    Hello World

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # This example requires the 'message_content' privileged intent to function.
 
 import discord
+import random
+import asyncio
 
 
 class MyClient(discord.Client):
@@ -13,8 +15,26 @@ class MyClient(discord.Client):
         if message.author.id == self.user.id:
             return
 
-        if message.content.startswith('!hello'):
-            await message.reply('Hello!', mention_author=True)
+        if message.content.startswith('!嗨'):
+            await message.reply('銃三小 低能兒', mention_author=True)
+
+        if message.content.startswith('!猜數字'):
+            await message.channel.send('1~10猜個數字')
+
+            def is_correct(m):
+                return m.author == message.author and m.content.isdigit()
+
+            answer = random.randint(1, 10)
+
+            try:
+                guess = await self.wait_for('message', check=is_correct, timeout=5.0)
+            except asyncio.TimeoutError:
+                return await message.channel.send(f'要不要打字啦 操 答案:{answer}.')
+
+            if int(guess.content) == answer:
+                await message.channel.send('對了啦 阿不就好棒棒')
+            else:
+                await message.channel.send(f'笨小孩是{answer}啦幹')
 
 
 intents = discord.Intents.default()
